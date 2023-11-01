81866cbb149c3e26fd8cce120017fc6a047eb7b3
d0422 <rlfehd2013@naver.com>
2023-11-01 09:54:15 +0900

    refactor: Text컴포넌트 적용

--- src/components/Sections/Rollling/Rolling.tsx
+++ src/components/Sections/Rollling/Rolling.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { StyleOption } from '../../../type';
 import { Icon } from './Icon';
 import { styleOptions } from './rollingStyle';
+import { Text } from '@/components/common/Text';
 const ICONS = [
   'blind',
   'call',
@@ -32,17 +33,15 @@ export const Rolling = () => {
 
   return (
     <div className="w-full h-[130vw] flex flex-col justify-center items-center">
-      <div className="text-[#ffffff] text-[4vw] mt-[16%] mb-[10%] flex justify-center">
-        1초만에 교환하세요.
+      <div className="mt-[16%] mb-[10%]">
+        <Text>1초만에 교환하세요.</Text>
       </div>
       <div className="relative mb-[10%] pc:w-[55vw] pc:h-[55vw] mobile:w-[80vw] mobile:h-[80vw]">
         {ICONS.map((icon) => `/icons/${icon}.png`).map((icon, i) => (
           <Icon key={i} src={icon} style={styleArray[i]} />
         ))}
       </div>
-      <div className="text-[#ffffff] text-[4vw] flex justify-center">
-        무엇이든
-      </div>
+      <Text>무엇이든</Text>
     </div>
   );
 };
