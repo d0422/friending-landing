a69d5f7fb2a235ba4be44b947a2389853d4d5c38
d0422 <rlfehd2013@naver.com>
2023-11-02 11:01:47 +0900

    chore: 카드 디자인 수정

--- src/components/Sections/cardInfo.ts
+++ src/components/Sections/cardInfo.ts
@@ -4,39 +4,39 @@ export const cardsInfo: CardInfo[] = [
   {
     src: '/profiles/profile1.png',
     rotateInfo: {
-      rotateY: '-50deg',
-      translateX: '-5vw',
-      translateZ: '5vw',
+      rotateY: '-40deg',
+      translateX: '-7vw',
+      translateZ: '1vw',
     },
   },
   {
     src: '/profiles/profile2.png',
     rotateInfo: {
-      rotateY: '-15deg',
-      translateZ: '9vw',
+      rotateY: '-10deg',
+      translateZ: '4vw',
       translateX: '-4vw',
     },
   },
   {
     src: '/profiles/profile5.png',
     rotateInfo: {
-      translateZ: '9.5vw',
+      translateZ: '5vw',
     },
   },
   {
     src: '/profiles/profile3.png',
     rotateInfo: {
-      rotateY: '15deg',
-      translateZ: '9vw',
+      rotateY: '10deg',
+      translateZ: '4vw',
       translateX: '4vw',
     },
   },
   {
     src: '/profiles/profile4.png',
     rotateInfo: {
-      rotateY: '50deg',
-      translateX: '5vw',
-      translateZ: '5vw',
+      rotateY: '40deg',
+      translateX: '7vw',
+      translateZ: '1vw',
     },
   },
 ];
