bde52fa67e08127e7e4c152f0cb91afc731369ab
d0422 <rlfehd2013@naver.com>
2023-11-02 11:36:46 +0900

    chore: 롤링 값 보정 및 사이즈 변경

--- src/components/Sections/Rollling/Icon.tsx
+++ src/components/Sections/Rollling/Icon.tsx
@@ -11,16 +11,16 @@ export const Icon = ({ src, style }: { src: string; style: StyleOption }) => {
         right: style.position?.right,
         bottom: style.position?.bottom,
         filter: `brightness(${style.brightness}%)`,
-        transform: `scale(${style.center ? '1.2' : 1})`,
+        transform: `scale(${style.center ? '1.3' : 1})`,
         transition: 'all 1s ',
       }}
     >
       <div
-        className={`relative pc:w-[8vw] pc:h-[8vw] mobile:w-[12vw] mobile:h-[12vw]`}
+        className={`relative pc:w-[8.5vw] pc:h-[8.5vw] mobile:w-[12vw] mobile:h-[12vw]`}
       >
         <Image src={src} alt="icon" layout="fill" objectFit="contain" />
         <div
-          className="pc:w-[5vw] pc:h-[5vw] mobile:w-[9vw] mobile:h-[9vw]"
+          className="pc:w-[6.5vw] pc:h-[6.5vw] mobile:w-[9vw] mobile:h-[9vw]"
           style={{
             position: 'absolute',
             left: '50%',
--- src/components/Sections/Rollling/rollingStyle.ts
+++ src/components/Sections/Rollling/rollingStyle.ts
@@ -1,14 +1,14 @@
 import { StyleOption } from '@/type';
 
 const DEFAULT_VW = 16.6;
-
+const CORRECT_VALUE = 2;
 export const styleOptions: StyleOption[] = [
   {
     position: { top: `${DEFAULT_VW * 2}%`, left: 0 },
     brightness: 75,
   },
   {
-    position: { top: `${DEFAULT_VW}%`, left: `${DEFAULT_VW}%` },
+    position: { top: `${DEFAULT_VW}%`, left: `${DEFAULT_VW - CORRECT_VALUE}%` },
     brightness: 90,
   },
   {
@@ -17,7 +17,10 @@ export const styleOptions: StyleOption[] = [
     brightness: 100,
   },
   {
-    position: { top: `${DEFAULT_VW}%`, left: `${DEFAULT_VW * 4}%` },
+    position: {
+      top: `${DEFAULT_VW}%`,
+      left: `${DEFAULT_VW * 4 + CORRECT_VALUE}%`,
+    },
     brightness: 90,
   },
   {
@@ -29,19 +32,31 @@ export const styleOptions: StyleOption[] = [
     brightness: 50,
   },
   {
-    position: { top: `${DEFAULT_VW * 4}%`, left: `${DEFAULT_VW * 4}%` },
+    position: {
+      top: `${DEFAULT_VW * 4}%`,
+      left: `${DEFAULT_VW * 4 + CORRECT_VALUE}%`,
+    },
     brightness: 50,
   },
   {
-    position: { top: `${DEFAULT_VW * 5}%`, left: `${DEFAULT_VW * 3}%` },
+    position: {
+      top: `${DEFAULT_VW * 5}%`,
+      left: `${DEFAULT_VW * 3 + CORRECT_VALUE}%`,
+    },
     brightness: 25,
   },
   {
-    position: { top: `${DEFAULT_VW * 5}%`, left: `${DEFAULT_VW * 2}%` },
+    position: {
+      top: `${DEFAULT_VW * 5}%`,
+      left: `${DEFAULT_VW * 2 - CORRECT_VALUE}%`,
+    },
     brightness: 25,
   },
   {
-    position: { top: `${DEFAULT_VW * 4}%`, left: `${DEFAULT_VW}%` },
+    position: {
+      top: `${DEFAULT_VW * 4}%`,
+      left: `${DEFAULT_VW - CORRECT_VALUE}%`,
+    },
     brightness: 50,
   },
   { position: { top: `${DEFAULT_VW * 3}%`, left: 0 }, brightness: 50 },
