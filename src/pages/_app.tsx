83fcdd28b905b66b7df355964decdbe5abea5dad
d0422 <rlfehd2013@naver.com>
2023-11-04 09:38:31 +0900

    feat: og태그 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Friending" />
+        <meta name="keywords" content="프렌딩, SNS, 교환" />
+        <meta
+          property="og:description"
+          content="센스있는 프로필 트랜드 Friending!"
+        />
+        <meta property="og:url" content="https://friending.online/" />
+        <meta
+          property="og:image"
+          content="https://friending.online/ogLogo.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
